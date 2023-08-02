d76e97f05a967fdcb1329c76f86079973826a095
Aditya <dubeyaditya80045@gmail.com>
2023-08-02 18:47:34 +0530

    Added cheatsheet

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ faqItem.forEach(item => {
        item.classList.toggle('active');
     });
 });
+
+
+function scrollToTop(){
+    window.scrollTo({
+     top:0,
+     behavior:"smooth"
+    });
+}
+
+window.onscroll = function() {
+    var scrollToTopBtn = document.querySelector(".scroll-to-top-btn");
+    if (window.scrollY >= 300) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };  
+  
